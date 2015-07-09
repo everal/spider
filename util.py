d95d44d3c2807fe3d8c78b1c21296930b78f289e
Everal <everal.feng@gmail.com>
2015-07-09 18:17:29 +0800

    through len(line) filter content

--- util.py
+++ util.py
@@ -3,13 +3,13 @@
 
 import re
 def rm_tag(html):
-    f = open('tmp.txt', 'w')
+    f = open('temp.txt', 'w')
     s = ""
     r = re.compile('(?is)<body.*?>.*?</body>', re.M)
     m = r.search(html)
-    #m = re.match(r, html)
     if(m):
         s = m.group()
+    s = s.replace('<br />', '\n')
     s = re.sub(r'(?is)<script.*?>.*?</script>', '', s)
     s = re.sub(r'(?is)<style.*?>.*?</style>', '', s)
     s = re.sub(r'(?is)<!--.*?-->', '', s)
@@ -17,15 +17,15 @@ def rm_tag(html):
     s = s.replace('\t', '')
     s = s.replace(' ', '')
     s = s.replace('/^m', '')
-    print 'success'
     #s = re.sub(r'(<[^<>]+)\s*\n\s*','', s)
     l = s.split('\n')
-    print len(l)
+    #print len(l)
     for i in l: 
         if i.strip():
             if len(i) > 6:
                 f.write(i.strip() + '\n')
     f.close()
+    return s
 
 def get_para(html):
     f = open('tmp.txt', 'w')
@@ -45,9 +45,23 @@ def get_para(html):
             f.write(i.strip() + '\n')
     f.close()
 
+def filter_content(s):
+    f = open('a.txt', 'w')
+    para_list = s.split('\n')
+    avg = len(s)/len(para_list)
+    if(avg < 40):
+        avg = 40
+    if(avg > 80):
+        avg = 60
+    for i in range(len(para_list)):
+        if len(para_list[i]) > avg:
+            f.write(para_list[i].strip() + '\n')
+    f.close()
+
 if __name__ == "__main__":
     import urllib
-    c = urllib.urlopen('http://tech.163.com/13/1230/10/9HB88VE600094NRG.html')
+    c = urllib.urlopen('http://www.baidu.com/link?url=6q4fFojUjtgatTFIzAhn9iD-YeBMDSVrmhw40yYPPRA4qQooxKkQ-lmi4hMrTcg3AwDETiz70ezHBDkClYh0ba')
     html = c.read()
-    #rm_tag(html)
-    get_para(html)
+    s = rm_tag(html)
+    filter_content(s)
+    #get_para(html)
